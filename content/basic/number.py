b9782cb368b44d860303d5c2a1bbde51678f5b4b
lee <chic.sky.lee@gmail.com>
2018-10-05 23:01:57 +0800

    再次修改 number.py

--- content/basic/number.py
+++ content/basic/number.py
@@ -39,6 +39,23 @@
 # 格式化函数：format()
 
 
+# =====================================
+# 浮点数丢失精度问题：
+# 0.1+0.1+0.1-0.3 = 5.5511151231257827e-17
+# 1.2+2.4 = 3.5999999999999996
+# 1-2.0/3 = 0.33333333333333337
+# 3.1+2.3+3.4 = 8.8000000000000007
+
+# =====================================
+# 注意：
+# 如果在Python2中引入了__future__模块的division
+# 那么Python2中的除法，将类似于Python3中的除法，
+# 请在Python2中比较：
+# print(5/2)
+# 和
+# from __future__ import division
+# print(5/2)
+# =====================================
 # 其它相关内容：
 # 注意：这些内容将留待模块部分介绍
 # 模块：math、decimal、fractions、statistics、random、difflib
@@ -69,6 +86,9 @@ def show_operators_for_number():
 
 
 # 进制转换
+# 数学上的十进制转换二进制的思路是：除2取余，逆序排列
+# 方法：将一个数字依次除以2，将余数写在右侧。最后从最下面依次写出即可。
+# 按二进制位进行运算
 def hexadecimal_conversion():
     # 10进制转16进制
     print(hex(18))  # 0x12
@@ -123,6 +143,46 @@ def show_operators_only_for_integer():
     # 00000000101----补齐11位的5的二进制形式
     # 10100000000----将每个1向左移8位，得到1280的二进制形式
     print(int('10100000000', 2)) # 1280
+
+
+# =====================================
+# 查看变量的id
+def look_id():
+    int_a = 3
+    int_b = 5
+    print('查看变量的id：')
+    print(id(int_a))
+    print(id(int_b))
+
+# is的特性：对于-5~256之间的值用is判断不同变量值，结果为True
+def is_feature00():
+    for i in range(-10, 260, 1):
+        print(i)
+        a = i
+        b = i + 1 - 1
+        print('a is b:', a is b)
+        print('id(a):{}, id(b):{}'.format(id(a), id(b)))
+
+
+
+
+
+# 自带的数学函数
+def int_func():
+    # 次方
+    print('pow(4, 3)：{}'.format(pow(4, 3)))
+    print('4**3：{}'.format(4**3))
+    # 绝对值
+    print('abs(-2.6)：{}'.format(abs(-2.6)))
+    # 圆整（向下取整，即不四舍五入，直接截断取整）
+    # 注意：需要四舍五入，请用math.ceil()函数
+    print('round(2.5)：{}'.format(round(2.5)))
+    print('round(2.7)：{}'.format(round(2.7)))
+    print('round(1.234567, 3)：{}'.format(round(1.234567, 3)))
+    print('round(2.3)：{}'.format(round(2.3)))
+    # 返回一个二元素元组，代表商和余数
+    print('divmod(5,3)：{}'.format(divmod(5,3)))
+
 
 
 if __name__ == '__main__':
